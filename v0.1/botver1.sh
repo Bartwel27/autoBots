e4e411a13177c0c2d818c18d7956a00ad1ea9cd1
Bartwel27 <81015407+Bartwel27@users.noreply.github.com>
2024-03-20 22:20:55 +0200

    Add files via upload

---
+++ v0.1/botver1.sh
@@ -0,0 +1,42 @@
+#!/system/bin/sh
+
+# links
+afloat0="git clone https://github.com/afloat0/mike"
+afloat01="git clone https://github.com/afloat01/mike"
+pfamer="git clone https://github.com/teddy1000/persistent-farmer"
+g_hits="git clone https://github.com/go-hits/rachel"
+zedp="git clone https://github.com/zedpathmusik/D-BLESS-ft-UNKNOWN-ARTIST-_-AKADUNKA-SAMPLE"
+bc="git clone https://github.com/bee02/corp"
+ser="git clone https://github.com/bartwel27/server"
+
+
+# files variables
+af0_f=mike
+af01_f=mike
+pf_f=persistent-farmer
+gh_f=rachel
+zp_f=D-BLESS-ft-UNKNOWN-ARTIST-_-AKADUNKA-SAMPLE
+bc_f=corp
+ser_f=server
+
+vr_f=vote-rank
+fb_f=facebook
+sfb_f=secure-facebook
+nf_f=netflix
+mw_f=movies-website
+m_f=microsoft
+s_f=steam
+g_f=google
+f_f=food
+adv_f=advanced-vote
+
+
+$afloat0
+
+mkdir fileDB
+mv -t fileDB mike
+
+echo "the first file named $af0 was cloned and was moved to fileDB [+]"
+
+$afloat01
+$zedp
